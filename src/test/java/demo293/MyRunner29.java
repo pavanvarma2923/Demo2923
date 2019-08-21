c71353018c19e07323d6e014f661306eadf60ee6
pavanvarma2923 <54269091+pavanvarma2923@users.noreply.github.com>
2019-08-21 15:06:53 +0530

    Update MyRunner29.java

--- src/test/java/demo293/MyRunner29.java
+++ src/test/java/demo293/MyRunner29.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="C:\\Users\\training_h2a.06.15\\Desktop\\selenium workspace\\Demo2923\\Feature4567",glue="demo293",plugin= {"html:target/cucumber-htmlreport.html","json:target/cucumber.json"})
+@CucumberOptions(json:target/cucumber.json)
 
 public class MyRunner29 {
 	
